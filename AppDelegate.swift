3efcced9a6f8874e25488a6c084bae3b52b5f7e6
dabest822 <120066161+dabest822@users.noreply.github.com>
2025-06-17 11:19:59 -0400

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,31 @@
+import Cocoa
+import SpriteKit
+
+@NSApplicationMain class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var view: SKView!
+    @IBOutlet weak var window: NSWindow!
+
+    @objc func applicationDidFinishLaunching(_ notification: Notification) {
+        window.contentAspectRatio = NESScene.screenSize
+        window.contentMinSize = NESScene.screenSize
+
+        if let path = ProcessInfo.processInfo.environment["file"] {
+            print(path)
+            open(path: path)
+        }
+    }
+
+    @objc func application(_ sender: NSApplication, openFile path: String) -> Bool {
+        open(path: path)
+
+        return true
+    }
+
+    func open(path: String) {
+        let scene = NESScene(file: path)
+
+        view!.presentScene(scene)
+        view!.ignoresSiblingOrder = true
+        view!.showsFPS = true
+    }
+}
