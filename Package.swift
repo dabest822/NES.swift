9289f56d98ea79ed20924eef3d90e62bacc5c097
dabest822 <120066161+dabest822@users.noreply.github.com>
2025-06-17 11:53:55 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "NES",
     defaultLocalization: "en",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(name: "NES", targets: ["NES"])
     ],
